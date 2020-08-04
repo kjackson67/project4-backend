d7922031081ee0261d6c54315fd72636c84d4069
“Korby <jacksonkorbyr@johndeere.com>
2020-08-04 09:18:40 -0500

    updated users profile.ejs page

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -7,23 +7,23 @@ const UserModel = require("../models").User;
 const RoundModel = require("../models").Round;
 
 // Index = Render/Get all courses //
-// router.get("/", (req, res) => {
-//     res.render("index.ejs", {           // *** // CourseModel.findAll().then((allCourseFromDB) => {
-//     });
-// });                                    // *** // course: allCourseFromDB,
+router.get("/", (req, res) => {
+    res.render("index.ejs", {               // *** // CourseModel.findAll().then((allCourseFromDB) => {
+    });
+});                                         // *** // course: allCourseFromDB,
         
 
 //  NEW ROUTE - EMPTY FORM   // 
-// router.get("/new", (req, res) => {
-//     res.render("new.ejs");
-//   });
+router.get("/new", (req, res) => {
+    res.render("new.ejs");
+  });
 
 // Create New "POST" Route - render "Create" W/SEQUELIZE  //
-// router.post("/", (req, res) => {                                    // if (req.body.readyToEat === "on") {
-//     CourseModel.create(req.body).then((newCourse) => {              //   req.body.readyToEat = true;
-//     res.redirect("/courses");                                       //   req.body.readyToEat = false;
-//     });                                                             // }
-// });
+router.post("/", (req, res) => {                                    // if (req.body.readyToEat === "on") {
+    CourseModel.create(req.body).then((newCourse) => {              //   req.body.readyToEat = true;
+    res.redirect("/courses");                                       //   req.body.readyToEat = false;
+    });                                                             // }
+});
 
 router.get("/:id/edit", function (req, res) {
     CourseModel.findByPk(req.params.id).then((courseToEdit) => {
@@ -33,40 +33,40 @@ router.get("/:id/edit", function (req, res) {
         });
     });
 
-// router.get("/:id", (req, res) => {
-//     CourseModel.findByPk(req.params.id, {
-//         include: [{
-//             model: User,
-//             attribute: ["name"],
-//         },
-//         {
-//             model: Round,
-//         },
-//         ],
-//             attributes: ["name", "location", "par", "img", "userId"]
-//     })
-//     .then(course => {
-//         console.log(course)
-//         res.render("show.ejs", {
-//             course: course
-//         });
-//     });
-// });
+router.get("/:id", (req, res) => {
+    CourseModel.findByPk(req.params.id, {
+        include: [{
+            model: User,
+            attribute: ["name"],
+        },
+        {
+            model: Round,
+        },
+        ],
+            attributes: ["name", "location", "par", "img", "userId"]
+    })
+    .then(course => {
+        console.log(course)
+        res.render("show.ejs", {
+            course: course
+        });
+    });
+});
 
-// router.put('/:id', (req, res) => {
-//     CourseModel.update(req.body, {where: {id: req.params.id },
-//         returning: true,
-//   }).then((updatedCourse) => {
-//         res.redirect("/course");
-//         });
-//     });
+router.put('/:id', (req, res) => {
+    CourseModel.update(req.body, {where: {id: req.params.id },
+        returning: true,
+  }).then((updatedCourse) => {
+        res.redirect("/course");
+        });
+    });
 
 
-// router.delete("/:id", (req, res) => {
-//     CourseModel.destroy({ where: { id: req.params.id } }).then(() => {
-//       res.redirect("/courses");
-//     });
-//   });
+router.delete("/:id", (req, res) => {
+    CourseModel.destroy({ where: { id: req.params.id } }).then(() => {
+      res.redirect("/courses");
+    });
+  });
 
 module.exports = router;                  
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const UserModel = require("../models").User;
-const CourseModel = reuire("../models").Course;
+const CourseModel = require("../models").Course;
 
 // GET USERS PROFILE
 router.get("/profile/:id", (req, res) => {
