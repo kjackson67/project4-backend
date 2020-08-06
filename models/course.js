cd623803a9c144938753f4d088fc7282c1b3a56c
“Korby <jacksonkorbyr@johndeere.com>
2020-08-06 14:09:53 -0500

    updated edit.ejs

--- models/course.js
+++ models/course.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      CourseModel.belongsToMany(models.User, {                      // define association here
+      CourseModel.belongsToMany(models.User, {
         through: "PlayerCourse",
         foreignKey: "courseId",
         otherKey: "userId"
