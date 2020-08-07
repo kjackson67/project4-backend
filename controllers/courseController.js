5e4edcdc4e0db1e2b4c5193326016f12b008b4e7
“Korby <jacksonkorbyr@johndeere.com>
2020-08-07 00:40:23 -0500

    updated all views and userController

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -7,24 +7,72 @@ const CourseModel = require("../models").Course;
 const UserModel = require("../models").User;
 // const RoundModel = require("../models").Round;
 
-     //  NEW ROUTE - EMPTY FORM   // 
-router.get("/new", (req, res) => {
-    res.render("new.ejs");
-});
+// router.get("/:index", (req, res) => {
+//     console.log(req.params.index);
+//     res.render("show.ejs", {
+//     pokemon: pokemon[req.params.index],
+//     index: req.params.index
+//     });
+// });
+
 
 // Index = Render/Get all courses //
 router.get("/", (req, res) => {
-    CourseModel.findAll().then((allCourseFromDB) => {                 // *** // CourseModel.findAll().then((allCourseFromDB) => {
+    CourseModel.findAll().then((allCoursesFromDB) => {                 // *** // CourseModel.findAll().then((allCourseFromDB) => {
         res.render("index.ejs", {
-        courses: allCourseFromDB,                                        
+        course: allCoursesFromDB,                                        
         });
     });
 });
 
+     //  NEW ROUTE - EMPTY FORM   // 
+     router.get("/new", (req, res) => {
+        res.render("new.ejs");
+    });
+    
+    // Create New "POST" Route - render "Create" W/SEQUELIZE  //
+router.post("/", (req, res) => {                                    // if (req.body.readyToEat === "on") {
+    CourseModel.create(req.body).then((newCourse) => {              //   req.body.readyToEat = true;
+    res.redirect("/course");                                       //   req.body.readyToEat = false;
+    });                                                             // }
+});
+
+router.put('/:id', (req, res) => {
+    console.log(req.body);
+    CourseModel.update(req.body, { where: { id: req.params.id },
+    returning: true, 
+    }).then((updatedCourse) => {
+        User.findByPk(req.body.season).then((foundUser) => {
+            Course.findByPk(req.params.id).then((foundCourse) => {
+                foundCourse.addCourse(foundUser);
+                res.redirect("/course");
+            });
+        });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    CourseModel.destroy({ where: { id: req.params.id } }).then(() => {
+      res.redirect("/courses");
+    });
+  });
+
+  router.get("/:id/edit", function (req, res) {
+    CourseModel.findByPk(req.params.id).then((foundCourse) => {
+        // User.findAll().then((allUsers) => {                                  // RoundModel.findAll().then((allRounds) => {
+            res.render("edit.ejs", {
+                course: foundcourse,
+                // users: allUsers,
+            });
+        });
+    });                                          
+// });
+
+
 router.get("/:id", (req, res) => {
     CourseModel.findByPk(req.params.id, {
         include: [{
-            model: Player,
+            model: User,
             attributes: ["name"],
         },
         ],
@@ -37,45 +85,11 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// Create New "POST" Route - render "Create" W/SEQUELIZE  //
-router.post("/", (req, res) => {                                    // if (req.body.readyToEat === "on") {
-    CourseModel.create(req.body).then((newCourse) => {              //   req.body.readyToEat = true;
-    res.redirect("/courses");                                       //   req.body.readyToEat = false;
-    });                                                             // }
-});
-
 
 //  NEW ROUTE - EMPTY FORM   // 
-router.get("/new", (req, res) => {
-    res.render("new.ejs");
-  });
-
-
-router.get("/:id/edit", function (req, res) {
-    CourseModel.findByPk(req.params.id).then((courseToEdit) => {
-      res.render("edit.ejs", {                                            // RoundModel.findAll().then((allRounds) => {
-        course: courseToEdit,
-        });
-    });                                          
-});
-                
-
-router.put('/:id', (req, res) => {
-    CourseModel.update(req.body, { where: { id: req.params.id },
-    returning: true, 
-}).then((updatedCourse) => {
-    console.log(updatedCourse);
-        res.redirect("/courses");
-        }
-    );
-});
-
-
-router.delete("/:id", (req, res) => {
-    CourseModel.destroy({ where: { id: req.params.id } }).then(() => {
-      res.redirect("/courses");
-    });
-  });
+// router.get("/new", (req, res) => {
+//     res.render("new.ejs");
+//   });
 
 module.exports = router;                  
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,50 +5,122 @@ const UserModel = require("../models").User;
 const CourseModel = require("../models").Course;
 // const RoundModel = require("../models").Round;
 
-// GET USERS PROFILE
+router.get("/", (req, res) => {
+  res.render("user/index.ejs");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("user/signup.ejs");
+});
+
+router.post("/", (req, res) => {
+  UserModel.create(req.body).then((newUser) => {
+    res.redirect(`/user/profile/${newUser.id}`);
+  });
+});
+
+router.get("/login", (req, res) => {
+  res.render("user/login.ejs");
+});
+
+router.post("/login", (req, res) => {
+  UserModel.findOne({
+    where: {
+      username: req.body.username,
+      password: req.body.password,
+    },
+  })
+  .then((loggedInUser) => {
+    res.redirect(`/user/profile/${loggedInUser.id}`);
+  })
+  .catch((err) => {
+    res.redirect("/user");
+  });
+});
+
 router.get("/profile/:id", (req, res) => {
-    UserModel.findByPk(req.params.id, {
-      include: [
-        {
-          model: CourseModel,
-          attributes: ["id", "courseName"],
-        },
-      ],
-    }).then((userProfile) => {
-      res.render("users/profile.ejs", {
-        user: userProfile,
-      });
+  UserModel.findByPk(req.params.id, {
+      res.render("user/profile.ejs", {
+      }),
     });
   });
 
-   // SIGNUP //
-//  router.get("/signup", (req, res) => {
-//   res.render("users/signup.ejs");
-// });
-
-// router.post("/login", (req, res) => {
-//   UserrModel.findOne({ where: {username: req.body.username, password: req.body.password,
-//   } }).then((logInUser) => {
-//       res.redirect(`/users/profile/${logInUser.id}`);
-//   })
-//   .catch((err) => {
-//       res.redirect("/users");
-//   });
-// });
-
-
-  // UPDATE PROFILE - SEQUELIZE(.PUT) //
-router.put("/profile/:id", (req, res) => {
-    UserModel.update(req.body, {where: { id: req.params.id } }).then(() => {
-      res.redirect(`/users/profile/${req.params.id}`);
+  router.put("/profile/:id", (req, res) => {
+    UserModel.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+      plain: true,
+    }).then((updatedUser) => {
+      res.redirect(`/user/profile/${req.params.id}`);
     });
   });
-  
-  // DELETE USER ROUTE/PROFILE //
+
   router.delete("/:id", (req, res) => {
     UserModel.destroy({ where: { id: req.params.id } }).then(() => {
-      res.redirect("/");
+      res.redirect("/user");
     });
   });
   
   module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// // GET USERS PROFILE
+// router.get("/profile/:id", (req, res) => {
+//     UserModel.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: CourseModel,
+//           attributes: ["id", "courseName"],
+//         },
+//       ],
+//     }).then((userProfile) => {
+//       res.render("users/profile.ejs", {
+//         user: userProfile,
+//       });
+//     });
+//   });
+
+//    // SIGNUP //
+// //  router.get("/signup", (req, res) => {
+// //   res.render("users/signup.ejs");
+// // });
+
+// // router.post("/login", (req, res) => {
+// //   UserrModel.findOne({ where: {username: req.body.username, password: req.body.password,
+// //   } }).then((logInUser) => {
+// //       res.redirect(`/users/profile/${logInUser.id}`);
+// //   })
+// //   .catch((err) => {
+// //       res.redirect("/users");
+// //   });
+// // });
+
+
+//   // UPDATE PROFILE - SEQUELIZE(.PUT) //
+// router.put("/profile/:id", (req, res) => {
+//     UserModel.update(req.body, {where: { id: req.params.id } }).then(() => {
+//       res.redirect(`/users/profile/${req.params.id}`);
+//     });
+//   });
+  
+//   // DELETE USER ROUTE/PROFILE //
+//   router.delete("/:id", (req, res) => {
+//     UserModel.destroy({ where: { id: req.params.id } }).then(() => {
+//       res.redirect("/");
+//     });
+//   });
+  
+//   module.exports = router;
